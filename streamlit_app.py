a848bb5cffc0e1e8b1f2dbaf33c08fd876592e4c
ElmerDotti <42123943+ElmerDotti@users.noreply.github.com>
2025-03-31 17:12:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
+import streamlit as st
 from feature_selection import feature_selection_screen
 
-if __name__ == "__main__":
-    feature_selection_screen()
+st.set_page_config(page_title="Feature Selection CashMe", layout="wide")
+feature_selection_screen()
