a824f7f8f9354dac5c8c734620ce352913a410ec
ElmerDotti <42123943+ElmerDotti@users.noreply.github.com>
2025-03-31 17:19:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 from feature_selection import feature_selection_screen
 
-st.set_page_config(page_title="Feature Selection CashMe", layout="wide")
+# Título do App
+st.set_page_config(page_title="🏦 Desafio CashMe - Feature Selection", layout="wide")
+
+st.title("🏦 Desafio CashMe - Feature Selection")
+st.write("Este aplicativo permite realizar seleção de variáveis com técnicas avançadas de machine learning.")
+
+# Rodar a interface principal
 feature_selection_screen()
